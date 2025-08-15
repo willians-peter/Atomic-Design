c09b5818b100bd590109814ea8a735ba75daa51d
willians-peter <petercipriano@hotmail.com>
2025-08-14 21:25:23 -0300

    iniciando header e footer
    iniciando header e footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,32 @@ import Button from "./components/atoms/Button";
 import PlantIcon from "./components/atoms/PlantIcon";
 import PriceTag from "./components/atoms/PriceTag";
 import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
+import Header from "./components/organisms/Header/Header";
+import Footer from "./components/organisms/Footer/Footer";
+
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <p>PAGINA TRELLO</p>
-      <Button text="Botão Primário" variant="primary" />
-      <Button text="Botão Secundário" variant="secondary" />
-      <h1>
-        {" "}
-        Icone <PlantIcon size="lg" />{" "}
-      </h1>
-      <p>
-        {" "}
-        Valor do PriceTag: <PriceTag value={22.9} />{" "}
-      </p>
+    <div className="App">
+      <Header />
+      <main>
+        <div>
+          <p>PAGINA TRELLO</p>
+          <Button text="Botão Primário" variant="primary" />
+          <Button text="Botão Secundário" variant="secondary" />
+          <h1>
+            {" "}
+            Icone <PlantIcon size="lg" />{" "}
+          </h1>
+          <p>
+            {" "}
+            Valor do PriceTag: <PriceTag value={22.9} />{" "}
+          </p>
+        </div>
+
+        <Footer />
+      </main>
     </div>
   );
 }
---
+++ src/components/organisms/Header/Header.jsx
@@ -0,0 +1,27 @@
+import PlantStoreIcon from "../../assets/LocalFloristIcon.png";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="Header">
+      <div>
+        <img
+          src={PlantStoreIcon}
+          alt="Planta Store Icone"
+          style={{
+            borderRadius: "8px",
+            width: "32px",
+            height: "32px",
+            objectFit: "contain",
+            maxWidth: "32px",
+            maxHeight: "32px",
+          }}
+        />
+        <h1>Plant Store</h1>
+   
+      </div>
+    </header>
+  );
+}
+
+export default Header;
