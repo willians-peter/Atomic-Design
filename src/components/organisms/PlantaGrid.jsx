610f1952cfd78f3bcb80bf8fbdee7c356c68062d
willians-peter <petercipriano@hotmail.com>
2025-08-16 10:44:51 -0300

    exercicio 4
    use media query no plantagrid

--- src/components/organisms/PlantaGrid.jsx
+++ src/components/organisms/PlantaGrid.jsx
@@ -1,18 +1,22 @@
-import PlantaCard from "../molecules/PlantaCard";
-import styles from "./PlantaGrid.module.css";
+import React from 'react';
+import { Grid } from '@mui/material';
+import PlantaCard from '../molecules/PlantaCard';
 
-export default function PlantaGrid({ plantas = [] }) {
+const PlantaGrid = ({ plantas }) => {
   return (
-    <div className={styles.PlantaGridContainer}>
+    <Grid container spacing={2}>
       {plantas.map((planta) => (
-        <PlantaCard
-          key={planta.id}
-          name={planta.name}
-          imagem={planta.imagem}
-          price={planta.price}
-          categories={planta.categories}
-        />
+        <Grid item xs={12} sm={6} md={4} key={planta.id} sx={{ display: 'flex', justifyContent: 'center' }}>
+          <PlantaCard
+            name={planta.name}
+            imagem={planta.imagem}
+            price={planta.price}
+            categories={planta.categories}
+          />
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
-}
+};
+
+export default PlantaGrid
